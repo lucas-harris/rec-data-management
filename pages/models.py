77d4fe6a102320adf4ba516d80ce559fb58b82aa
Lucas Harris <harrislr@miamioh.edu>
2018-08-15 16:38:21 -0400

    removed model default values

--- pages/models.py
+++ pages/models.py
@@ -1,39 +1,39 @@
 from django.db import models
 
 class Date(models.Model):
-    date = models.DateTimeField(primary_key=True, db_index=True, default='2018-01-01')
-    week = models.CharField(max_length=3, default='01')
-    year = models.CharField(max_length=4, default='2018')
-    month = models.CharField(max_length=3, default='1')
-    day_of_week = models.CharField(max_length=4, default='01')
-    day_of_month = models.CharField(max_length=3, default='01')
+    date = models.DateTimeField(primary_key=True, db_index=True)
+    week = models.CharField(max_length=3)
+    year = models.CharField(max_length=4)
+    month = models.CharField(max_length=3)
+    day_of_week = models.CharField(max_length=4)
+    day_of_month = models.CharField(max_length=3)
 
     def __str__(self):
         return str(self.date)
 
 class Data(models.Model):
-    key = models.CharField(primary_key=True, max_length=50, default='1')
+    key = models.CharField(primary_key=True, max_length=50)
     value = models.IntegerField(default = 0)
-    facility = models.CharField(max_length=10, default='rec')
-    area = models.CharField(max_length=10, default='fc')
-    time = models.CharField(max_length=5, default='0630')
-    gender = models.CharField(max_length=3, default='m')
+    facility = models.CharField(max_length=10)
+    area = models.CharField(max_length=10)
+    time = models.CharField(max_length=5)
+    gender = models.CharField(max_length=3)
     date = models.ForeignKey(Date, on_delete=models.CASCADE)
 
 class ChartSet(models.Model):
-    id = models.IntegerField(primary_key=True, default=1)
+    id = models.IntegerField(primary_key=True)
     saved = models.BooleanField(default=False)
     name = models.CharField(default='unsaved', max_length=60)
 
 class Chart(models.Model):
-    id = models.IntegerField(primary_key=True, db_index=False, default=1)
-    type = models.CharField(max_length=10, default='line')
-    title = models.CharField(max_length=50, default='title')
+    id = models.IntegerField(primary_key=True)
+    type = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
     chart_set = models.ForeignKey(ChartSet, on_delete=models.CASCADE)
     saved = models.BooleanField(default=False)
 
 class Graph(models.Model):
-    id = models.IntegerField(primary_key=True, default=1)
+    id = models.IntegerField(primary_key=True)
     chart = models.ForeignKey(Chart, on_delete=models.CASCADE)
     data = models.ManyToManyField('Data')
     color = models.CharField(max_length=25, default='rgb(255, 0, 0, .3')
